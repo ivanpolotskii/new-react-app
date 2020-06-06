47486a63e54285999f1edcdc5eb078df482d49de
Ivan <ivankickass03@gmail.com>
2020-06-06 21:02:07 +0300

    Просмотрено и выполнено задание 3

--- test.js
+++ test.js
@@ -1,59 +1,82 @@
-// const abbrevName=name=>name.split(' ').map(word=>word[0]).join('.').toUpperCase();
-
-// const sortReindeer = reindeerNames => console.log(reindeerNames.sort((a, b) => a.split(' ')[1].localeCompare(b.split(' ')[1])));
-
-// const getNiceNames=people=>people.filter(obj=>obj.wasNice===true).map(obj=>obj.name)
-// const getNaughtyNames=people=>people.filter(obj=>obj.wasNice===false).map(obj=>obj.name)
-
-// const greet =name=>'Hello '+(name[0].toUpperCase()+name.slice(1).toLowerCase())+'!'
-
-// const solve=(a,b)=>(a+b).split('').filter(lett=>(!a.includes(lett) || !b.includes(lett))).join('')
-
-// const divisibleByThree = str=> (str.split('').map(elem=>+elem).reduce((total,num)=>total+num,0))%3===0?true:false
-
-// const splitInParts =(s, partLength)=>{
-//     let chunks = [];
-//     for (let i = 0, charsLength = s.length; i < charsLength; i += partLength) {
-//         chunks.push(s.substring(i, i + partLength));
-//     };
-//     let result=chunks.join(' ');
-//     return result
-// }
-
-// const solution=digits=>{
-//     let answer=0;
-//     for(let i=0;i<digits.length;i++){
-//         if(answer<+digits.substr(i,5)){
-//             answer=+digits.substr(i,5);
-//         };
-//     };
-//     return answer;
-// };
-
-// const disemvowel = str =>{
-//     let glasn=['a','e','i','o','u'];
-//     str=str.split('').filter(el=>!glasn.includes(el.toLowerCase())).join('');
-//     return(str)
-// }
-
-
-
-let money = prompt('Ваш бюджет на месяц?', '');
-alert('Бюджет за 1 день:'+money/30);
+let money = +prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
-
-let statyaRashodov = prompt('Введите обязательную статью расходов в этом месяце','')
-let voSkolko = prompt('Во сколько обойдется?','')
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        statyaRashodov:voSkolko,
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 }
 
-debugger;
+// for (let i=0; i < 2; i++) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     }else{
+
+//     }
+// }
+let i=0;
+
+// while(i<2){
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     }else{
+
+//     }
+//     i++;
+// }
+do{
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    }else{
+
+    }
+    i++;
+}while(i<2);
+
+appData.moneyPerDay = appData.budget / 30;
+alert('Ежедневный бюджет:' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка')
+} else {
+    console.log('Произошла ошибка')
+}
+console.log(appData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
