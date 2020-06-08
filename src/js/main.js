d6bb91294d3260edd2fd41cacf75ddec25dc7628
Ivan <ivankickass03@gmail.com>
2020-06-08 13:52:23 +0300

    7 задание

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+let start=document.querySelector('#start');
+
+let budgetValue = document.querySelector('.budget-value');
+let daybudgetValue = document.querySelector('.daybudget-value');
+let levelValue = document.querySelector('.level-value');
+let expensesValue = document.querySelector('.expenses-value');
+let optionalexpensesValue = document.querySelector('.optionalexpenses-value');
+let incomeValue = document.querySelector('.income-value');
+let monthsavingsValue = document.querySelector('.monthsavings-value');
+let yearsavingsValue = document.querySelector('.yearsavings-value');
+
+let expensesItem=document.querySelectorAll('.expenses-item');
+
+let expensesItemBtn = document.querySelector('.expenses-item-btn');
+let optionalexpensesBtn = document.querySelector('.optionalexpenses-btn');
+let countBudgetBtn = document.querySelector('.count-budget-btn');
+
+let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+
+let chooseIncome = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let chooseSum = document.querySelector('#sum');
+let choosePercent = document.querySelector('#percent');
+
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
+
+console.log(expensesItem)
